8024f353733abf0ab6457575edd41a1a82c4d216
Oceane Villers <ocviller@paul-f3Ar5s6.clusters.42paris.fr>
2025-05-05 22:49:34 +0200

    hehe

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ocviller <ocviller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 21:49:51 by ocviller          #+#    #+#             */
-/*   Updated: 2025/05/05 22:27:20 by ocviller         ###   ########.fr       */
+/*   Updated: 2025/05/05 22:49:07 by ocviller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-    t_list	*temp;
+	t_list	*temp;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	while (lst->next != NULL)
 	{
 		temp = f(lst->content);
-        if (!temp)
-        {
-            ft_lstclear(&new, del);
-            return (NULL);
-        }
-        ft_lstadd_back(&new, temp);
-        lst = lst->next;
+		if (!temp)
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, temp);
+		lst = lst->next;
 	}
 	return (new);
 }
